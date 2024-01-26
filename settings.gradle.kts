747c4d0eaed35c0b00235ceccd81ee96794737de
Vladislav Sumin <vladislavsumin@mail.ru>
2024-01-26 20:29:23 +0300

    add gradle scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
+plugins {
+    // Плагин для публикации gradle scans
+    id("com.gradle.enterprise") version "3.16.2"
+}
+
 rootProject.name = "vs-control"
+
+val isCI = extra["ru.vs.core.ci"].toString().toBoolean()
+
+// Включаем публикацию gradle scans для всех билдов на CI
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(isCI)
+    }
+}
