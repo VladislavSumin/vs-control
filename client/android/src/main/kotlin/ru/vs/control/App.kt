8d3c202a19db04f2aeed5f607158e2b1d30c2eff
Vladislav Sumin <vladislavsumin@mail.ru>
2024-01-29 22:56:28 +0300

    add android client

---
+++ client/android/src/main/kotlin/ru/vs/control/App.kt
@@ -0,0 +1,5 @@
+package ru.vs.control
+
+import android.app.Application
+
+class App : Application()
---
+++ client/android/src/main/kotlin/ru/vs/control/ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package ru.vs.control.ui
+
+import android.app.Activity
+
+class MainActivity : Activity()
