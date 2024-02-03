cffd6d0ff0fd4a69fa70830b2965e038a8600aa5
Vladislav Sumin <vladislavsumin@mail.ru>
2024-02-03 19:24:35 +0300

    add todo

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("ru.vs.convention.analyze.detekt-build-logic")
 }
 
+// TODO убрать allprojects переместить в convention для модулей
 // Настраиваем детект для всех модулей
 allprojects {
     apply {
