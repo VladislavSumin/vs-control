6d41e6be4be5279b5daefa17eb633ac486693c4e
Владислав <vladislavsumin@mail.ru>
2024-02-17 00:11:37 +0300

    setup root screen for android target

--- client/android/src/main/kotlin/ru/vs/control/App.kt
+++ client/android/src/main/kotlin/ru/vs/control/App.kt
@@ -1,11 +1,17 @@
 package ru.vs.control
 
 import android.app.Application
+import org.kodein.di.DI
+import org.kodein.di.DIAware
 
-class App : Application() {
+class App : Application(), DIAware {
+    private var _di: DI? = null
+    override val di: DI
+        get() = _di!!
+
     override fun onCreate() {
         super.onCreate()
-        preInit()
+        _di = preInit().di
         InitLogger.i("App#onCreate()")
     }
 }
--- client/android/src/main/kotlin/ru/vs/control/ui/MainActivity.kt
+++ client/android/src/main/kotlin/ru/vs/control/ui/MainActivity.kt
@@ -3,13 +3,28 @@ package ru.vs.control.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import ru.vs.control.TestCompose
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.defaultComponentContext
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.android.closestDI
+import org.kodein.di.direct
+import org.kodein.di.instance
+import ru.vs.control.feature.rootScreen.ui.screen.rootScreen.RootScreenFactory
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), DIAware {
+    override val di: DI by closestDI()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val defaultContext = defaultComponentContext()
+        val rootComponentFactory = di.direct.instance<RootScreenFactory>()
+        val rootComponent = rootComponentFactory.create(defaultContext)
+
         setContent {
-            TestCompose()
+            rootComponent.Render(Modifier.fillMaxSize())
         }
     }
 }
