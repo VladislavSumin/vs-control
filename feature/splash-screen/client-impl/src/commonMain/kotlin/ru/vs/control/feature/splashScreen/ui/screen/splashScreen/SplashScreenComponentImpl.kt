4247db3ae82a938673a7670720ebc3393acc53d1
Владислав <vladislavsumin@mail.ru>
2024-02-22 23:28:37 +0300

    Add circular progress to Splash screen

--- feature/splash-screen/client-impl/src/commonMain/kotlin/ru/vs/control/feature/splashScreen/ui/screen/splashScreen/SplashScreenComponentImpl.kt
+++ feature/splash-screen/client-impl/src/commonMain/kotlin/ru/vs/control/feature/splashScreen/ui/screen/splashScreen/SplashScreenComponentImpl.kt
@@ -2,11 +2,16 @@ package ru.vs.control.feature.splashScreen.ui.screen.splashScreen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.ComponentContext
 import ru.vs.control.splashScreen.ui.screen.splashScreen.SplashScreenComponent
 
@@ -16,7 +21,14 @@ internal class SplashScreenComponentImpl(
     @Composable
     override fun Render(modifier: Modifier) {
         Box(modifier.background(Color.Cyan)) {
-            Text("Апка загружается", Modifier.align(Alignment.Center))
+            Column(
+                Modifier.align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text("Апка загружается")
+                Spacer(Modifier.height(8.dp))
+                CircularProgressIndicator()
+            }
         }
     }
 }
