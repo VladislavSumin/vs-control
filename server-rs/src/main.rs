ed67f714129d3a9aaade714ddcb1dc8b30556e6b
Владислав <vladislavsumin@mail.ru>
2024-06-12 11:13:19 +0300

    setup server hello world

--- server-rs/src/main.rs
+++ server-rs/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root));
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
