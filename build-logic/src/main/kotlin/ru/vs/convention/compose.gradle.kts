42cc66f21450c92c1a403c47b4202154f9b4cc72
Владислав <vladislavsumin@mail.ru>
2024-10-26 20:30:42 +0300

    remove old fix BuildScopeServices has been closed

--- build-logic/src/main/kotlin/ru/vs/convention/compose.gradle.kts
+++ build-logic/src/main/kotlin/ru/vs/convention/compose.gradle.kts
@@ -5,17 +5,8 @@ package ru.vs.convention
  */
 
 plugins {
-//    id("org.jetbrains.compose") apply false
-//    id("org.jetbrains.kotlin.plugin.compose") apply false
-}
-
-// TODO баг с "BuildScopeServices has been closed"
-// issue 1 (закрыто, но не исправлено) - https://github.com/JetBrains/compose-multiplatform/issues/3933
-// issue 2 (открыто) - https://github.com/gradle/gradle/issues/27099
-// На основе этого сделан этот workaround.
-if (project.name != "gradle-kotlin-dsl-accessors") {
-    plugins { apply { id("org.jetbrains.compose") } }
-    plugins { apply { id("org.jetbrains.kotlin.plugin.compose") } }
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 // val libs = rootProject.the<LibrariesForLibs>()
