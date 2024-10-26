f31909473764b02ff85fa8395a1fedee6d42b75a
Владислав <vladislavsumin@mail.ru>
2024-10-26 20:57:31 +0300

    add experimental compose ui tests

--- build-logic/src/main/kotlin/ru/vs/convention/compose.gradle.kts
+++ build-logic/src/main/kotlin/ru/vs/convention/compose.gradle.kts
@@ -1,10 +1,13 @@
 package ru.vs.convention
 
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
 /**
  * Настройки по умолчанию для jetbrains compose плагина.
  */
 
 plugins {
+    id("ru.vs.convention.kmp.common")
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
 }
@@ -17,3 +20,12 @@ plugins {
 //      // Игнорирование возможной несовместимости версий compose и версии языка kotlin
 //      kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=2.0.0-RC1")
 // }
+
+kotlin {
+    sourceSets {
+        commonTest.dependencies {
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
+        }
+    }
+}
