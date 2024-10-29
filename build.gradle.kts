9d9801337f33434eac14e718cff2741295e66e40
Владислав <vladislavsumin@mail.ru>
2024-10-30 02:30:33 +0300

    resolve gradle settings todo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,4 @@
 plugins {
-    id("ru.vs.convention.analyze.detekt-build-logic")
+    id("ru.vs.convention.analyze.detekt-all")
     id("ru.vs.convention.analyze.check-module-graph")
-}
-
-// TODO убрать allprojects переместить в convention для модулей
-// Настраиваем детект для всех модулей
-allprojects {
-    apply {
-        plugin("ru.vs.convention.analyze.detekt")
-    }
 }
