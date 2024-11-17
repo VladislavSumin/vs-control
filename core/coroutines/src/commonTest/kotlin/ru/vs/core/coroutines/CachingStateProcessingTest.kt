f14baf38218af8ebce286a4148e15eb9151cd0c4
Vladislav Sumin <v.sumin@vk.team>
2024-11-17 16:21:48 +0300

    add more tests

--- core/coroutines/src/commonTest/kotlin/ru/vs/core/coroutines/CachingStateProcessingTest.kt
+++ core/coroutines/src/commonTest/kotlin/ru/vs/core/coroutines/CachingStateProcessingTest.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalCoroutinesApi::class)
-// TODO дописать тесты.
 class CachingStateProcessingTest {
     /**
      * Проверяем что при отмене подписки все корректно отменяется.
@@ -53,6 +52,9 @@ class CachingStateProcessingTest {
         assertEquals(0, counter)
     }
 
+    /**
+     * Проверяем что старые подписки не перезапускаются при добавлении нового элемента
+     */
     @Test
     fun testCorrectProcessingAddNewElement() = runTest {
         val state = MutableStateFlow(listOf("A", "B", "C"))
@@ -78,6 +80,43 @@ class CachingStateProcessingTest {
         state.value = listOf("A", "NEW_ONE", "B", "C")
         runCurrent()
         assertEquals(4, counter)
+        job.cancel()
+    }
+
+    /**
+     * Проверяем что подписка старого элемента отменяется при его удалении
+     */
+    @Test
+    fun testCorrectProcessingRemoveNewElement() = runTest {
+        val state = MutableStateFlow(listOf("A", "B", "C"))
+
+        var counter = 0
+        var cancelCounter = 0
+
+        val job = launch {
+            coroutineScope {
+                state.cachingStateProcessing(
+                    keySelector = { it },
+                ) {
+                    try {
+                        counter++
+                        delay(Long.MAX_VALUE)
+                        emit(it)
+                    } finally {
+                        cancelCounter++
+                    }
+                }
+                    .stateIn(this)
+            }
+        }
+
+        assertEquals(0, counter)
+        runCurrent()
+        assertEquals(3, counter)
+        state.value = listOf("A", "C")
+        runCurrent()
+        assertEquals(3, counter)
+        assertEquals(1, cancelCounter)
         job.cancel()
     }
 }
