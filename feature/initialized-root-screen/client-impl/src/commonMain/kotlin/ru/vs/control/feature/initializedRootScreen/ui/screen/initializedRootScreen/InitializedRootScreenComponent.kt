143509bfb6ff4de5d4b57e902652bb88cb0f3e7d
Владислав <vladislavsumin@mail.ru>
2024-11-22 02:46:34 +0300

    add correct deeplink synchronous processing

--- feature/initialized-root-screen/client-impl/src/commonMain/kotlin/ru/vs/control/feature/initializedRootScreen/ui/screen/initializedRootScreen/InitializedRootScreenComponent.kt
+++ feature/initialized-root-screen/client-impl/src/commonMain/kotlin/ru/vs/control/feature/initializedRootScreen/ui/screen/initializedRootScreen/InitializedRootScreenComponent.kt
@@ -21,14 +21,25 @@ internal class InitializedRootScreenComponent(
 ) : Component(context), ComposeComponent {
 
     private val viewModel = viewModel { viewModelFactory.create() }
+
+    init {
+        // Пробуем обработать первый диплинк синхронно, до инициализации навигации, если таковой существует
+        // то мы должны сразу его обработать и при необходимости провести навигацию, так как иначе у нас откроется
+        // цепочка экранов по умолчанию и только потом произойдет переход по диплинку.
+        deeplink.tryReceive().getOrNull()?.also(viewModel::onDeeplink)
+        // Последующие диплинки уже можем обрабатывать не синхронно.
+        // Обратите внимание, из-за особенности работы корутин мы не можем быть уверены, что код внутри launch будет
+        // запущен синхронно, поэтому нам необходима явная проверка выше.
+        // Ответ на вопрос почему так - https://github.com/Kotlin/kotlinx.coroutines/issues/4276
+        launch { deeplink.consumeEach(viewModel::onDeeplink) }
+    }
+
+    // Вызывается после блока init выше так как мы хотим сначала обработать стартовый диплинк и только потом
+    // инициализировать навигацию.
     private val rootNavigation = childNavigationRoot(
         navigation = viewModel.navigation,
         onContentReady = onContentReady,
     )
-
-    init {
-        launch { deeplink.consumeEach(viewModel::onDeeplink) }
-    }
 
     @Composable
     override fun Render(modifier: Modifier) {
