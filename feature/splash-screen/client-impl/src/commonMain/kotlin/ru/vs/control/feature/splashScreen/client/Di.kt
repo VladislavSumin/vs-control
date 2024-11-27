c18c18e4fcb43a340a9aee5dffe853645320d7fb
Владислав <vladislavsumin@mail.ru>
2024-11-28 00:44:58 +0300

    add client package to navigation-root-screen

--- feature/splash-screen/client-impl/src/commonMain/kotlin/ru/vs/control/feature/splashScreen/client/Di.kt
+++ feature/splash-screen/client-impl/src/commonMain/kotlin/ru/vs/control/feature/splashScreen/client/Di.kt
@@ -2,9 +2,9 @@ package ru.vs.control.feature.splashScreen.client
 
 import org.kodein.di.DI
 import org.kodein.di.bindProvider
+import ru.vs.control.feature.splashScreen.client.ui.screen.splashScreen.SplashScreenFactory
 import ru.vs.control.feature.splashScreen.client.ui.screen.splashScreen.SplashScreenFactoryImpl
 import ru.vs.control.feature.splashScreen.client.ui.screen.splashScreen.SplashScreenViewModelFactory
-import ru.vs.control.feature.splashScreen.client.ui.screen.splashScreen.SplashScreenFactory
 import ru.vs.core.di.Modules
 
 fun Modules.featureSplashScreen() = DI.Module("feature-splash-screen") {
