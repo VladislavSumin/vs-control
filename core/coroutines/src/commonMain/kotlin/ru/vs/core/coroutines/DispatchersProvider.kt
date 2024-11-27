6f04d5d1ccbbf286aa94602136414595f26bd6fe
Владислав <vladislavsumin@mail.ru>
2024-11-28 01:08:00 +0300

    add io dispatcher

--- core/coroutines/src/commonMain/kotlin/ru/vs/core/coroutines/DispatchersProvider.kt
+++ core/coroutines/src/commonMain/kotlin/ru/vs/core/coroutines/DispatchersProvider.kt
@@ -8,13 +8,17 @@ import kotlinx.coroutines.MainCoroutineDispatcher
  * Абстракция над [Dispatchers], для возможности подменять [CoroutineDispatcher] в тестах.
  */
 interface DispatchersProvider {
+    val io: CoroutineDispatcher
     val default: CoroutineDispatcher
     val main: MainCoroutineDispatcher
     val unconfined: CoroutineDispatcher
 }
 
 internal object DefaultDispatchersProvider : DispatchersProvider {
+    override val io: CoroutineDispatcher = IoDispatcher
     override val default = Dispatchers.Default
     override val main = Dispatchers.Main
     override val unconfined = Dispatchers.Unconfined
 }
+
+internal expect val IoDispatcher: CoroutineDispatcher
---
+++ core/coroutines/src/jvmMain/kotlin/ru/vs/core/coroutines/DispatchersProvider.jvm.kt
@@ -0,0 +1,7 @@
+package ru.vs.core.coroutines
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+actual val IoDispatcher: CoroutineDispatcher
+    get() = Dispatchers.IO
