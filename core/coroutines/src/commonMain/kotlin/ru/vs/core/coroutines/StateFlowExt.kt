2e6da0cb66c3313e69f8b2c2c384c17f6f260c97
Vladislav Sumin <vladislavsumin@mail.ru>
2025-01-02 17:54:59 +0300

    add StateFlow.mapState function

---
+++ core/coroutines/src/commonMain/kotlin/ru/vs/core/coroutines/StateFlowExt.kt
@@ -0,0 +1,37 @@
+package ru.vs.core.coroutines
+
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+/**
+ * Работает аналогично [Flow.map], но возвращает [StateFlow].
+ */
+fun <T1, R> StateFlow<T1>.mapState(transform: (a: T1) -> R): StateFlow<R> {
+    return DerivedStateFlow(
+        getValue = { transform(this.value) },
+        flow = this.map { a -> transform(a) },
+    )
+}
+
+private class DerivedStateFlow<T>(
+    private val getValue: () -> T,
+    private val flow: Flow<T>,
+) : StateFlow<T> {
+
+    override val replayCache: List<T>
+        get() = listOf(value)
+
+    override val value: T
+        get() = getValue()
+
+    @InternalCoroutinesApi
+    override suspend fun collect(collector: FlowCollector<T>): Nothing {
+        coroutineScope { flow.distinctUntilChanged().stateIn(this).collect(collector) }
+    }
+}
