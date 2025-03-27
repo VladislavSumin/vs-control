5c4cd95ec8e7baf617c333b97e2b8cddb5be3282
Vladislav Sumin <vladislavsumin@mail.ru>
2025-03-28 00:08:56 +0300

    remove old navigation modules

--- core/navigation/factory-generator/api/build.gradle.kts
+++
@@ -1,3 +0,0 @@
-plugins {
-    id("ru.vs.convention.kmp.all")
-}
--- core/navigation/factory-generator/api/src/commonMain/kotlin/ru/vs/core/navigation/factoryGenerator/GenerateScreenFactory.kt
+++
@@ -1,5 +0,0 @@
-package ru.vs.core.navigation.factoryGenerator
-
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.CLASS)
-annotation class GenerateScreenFactory
