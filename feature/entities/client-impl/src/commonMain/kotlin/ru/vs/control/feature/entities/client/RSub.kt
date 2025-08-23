505690ea12d9f5380b40335a1397dd738bdbfc80
Vladislav Sumin <vladislavsumin@mail.ru>
2025-08-23 19:07:49 +0300

    add entites to server

---
+++ feature/entities/client-impl/src/commonMain/kotlin/ru/vs/control/feature/entities/client/RSub.kt
@@ -0,0 +1,6 @@
+@file:RSubClientInterfaces([EntitiesRsub::class])
+
+package ru.vs.control.feature.entities.client
+
+import ru.vs.control.feature.entities.rsub.EntitiesRsub
+import ru.vs.rsub.RSubClientInterfaces
