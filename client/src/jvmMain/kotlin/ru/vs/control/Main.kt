8f3e1b6835ba48ab38441d7c305ed1a837cc0d32
Vladislav Sumin <vladislavsumin@mail.ru>
2025-09-27 23:29:13 +0300

    use runOnUiThread from core

--- client/src/jvmMain/kotlin/ru/vs/control/Main.kt
+++ client/src/jvmMain/kotlin/ru/vs/control/Main.kt
@@ -12,8 +12,8 @@ import org.kodein.di.instance
 import ru.vs.control.feature.appInfo.client.domain.AppInfoInteractor
 import ru.vs.control.feature.rootScreen.client.ui.screen.rootScreen.RootScreenFactory
 import ru.vs.core.decompose.context.DefaultVsComponentContext
-import javax.swing.SwingUtilities
 import kotlin.system.exitProcess
+import ru.vladislavsumin.core.decompose.compose.runOnUiThread
 
 /**
  * Точка входа в приложение.
@@ -47,33 +47,6 @@ fun main() {
             rootComponent.Render(Modifier.fillMaxSize())
         }
     }
-}
-
-/**
- * See https://arkivanov.github.io/Decompose/getting-started/quick-start/
- * See https://github.com/arkivanov/Decompose/blob/master/sample/app-desktop/src/jvmMain/kotlin/com/arkivanov/sample/app/Utils.kt
- */
-@Suppress("TooGenericExceptionCaught")
-internal fun <T> runOnUiThread(block: () -> T): T {
-    if (SwingUtilities.isEventDispatchThread()) {
-        return block()
-    }
-
-    var error: Throwable? = null
-    var result: T? = null
-
-    SwingUtilities.invokeAndWait {
-        try {
-            result = block()
-        } catch (e: Throwable) {
-            error = e
-        }
-    }
-
-    error?.also { throw it }
-
-    @Suppress("UNCHECKED_CAST")
-    return result as T
 }
 
 fun setExitOnUncaughtException() {
